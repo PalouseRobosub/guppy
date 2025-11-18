a8711324a20a580a9dda92a2d7aa428842729188
Cole Wilson <colewilson.main@gmail.com>
2025-11-18 17:21:01 +0000

    fix: fixed optimization function in thrust vectoring

--- src/chassis_control/chassis_control/thrust_vectoring.py
+++ src/chassis_control/chassis_control/thrust_vectoring.py
@@ -6,14 +6,14 @@ from geometry_msgs.msg import Twist
 
 from math import sin, cos, tan, radians
 import numpy as np
-from scipy.optimize import lsq_linear
+from scipy.optimize import lsq_linear, minimize
 
 motors = [
     # corners
-    (-1, +1, 0,  0, -135),
-    (+1, +1, 0,  0, +135),
-    (+1, -1, 0,  0,  +45),
-    (-1, -1, 0,  0,  -45),
+    (-1, +1, 0,  0, +135),
+    (+1, +1, 0,  0, -135),
+    (+1, -1, 0,  0,  -45),
+    (-1, -1, 0,  0,  +45),
 
     # middles
     (+0, +1, 0, 90,    0),
@@ -53,11 +53,11 @@ class ThrustVectoring(Node):
         super().__init__('thrust_vectoring')
 
         self.subscription = self.create_subscription(Twist, '/teleop/chassis_twist', self.callback, 10)
-        self.thrust_publisher = self.create_publisher(Float32MultiArray, '/chassis_control/motor_thrusts', 10)
+        self.thrust_publisher = self.create_publisher(Float32MultiArray, '/chassis_control/motor_outputs', 10)
         self.twist_publisher = self.create_publisher(Twist, '/chassis_control/output_twist', 10)
 
     def callback(self, msg):
-        # self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%s"' % str(msg))
         desired = np.array([
                 msg.linear.x,
                 msg.linear.y,
@@ -66,16 +66,30 @@ class ThrustVectoring(Node):
                 msg.angular.y,
                 msg.angular.z
         ])
+        self.get_logger().info('desired: "%s"' % str(desired))
 
-        bounds = ([-5]*len(motors), [6]*len(motors))
+        bounds = ([-1]*len(motors), [1]*len(motors))
 
-        solutions = lsq_linear(coefficient_matrix, desired, bounds=bounds).x
+        solutions = np.linalg.lstsq(coefficient_matrix, desired, rcond=None)[0]
+        def fun(x):
+            s = sum(np.abs(np.dot(coefficient_matrix,x)-desired))
+            return s
+        try:
+            sol = minimize(fun, solutions, method='L-BFGS-B', bounds = [(-1,1) for i in range(len(motors))])
+        except ValueError as e:
+            sol = {"x":[0]*len(motors)}
+        solutions = sol['x']
+
+        #solutions = lsq_linear(coefficient_matrix, desired, bounds=bounds).x
+        self.get_logger().info('solutions: "%s"' % str(solutions))
 
         out_thrusts = Float32MultiArray()
-        out_thrusts.data = solutions
+        out_thrusts.data = solutions.tolist()
+        self.get_logger().info('out: "%s"' % str(out_thrusts))
         self.thrust_publisher.publish(out_thrusts)
 
-        rA = solutions * coefficient_matrix
+        rA = list(map(sum, (solutions * coefficient_matrix).tolist()))
+        self.get_logger().info('rA: "%s"' % str(rA))
 
         out_twist = Twist()
         out_twist.linear.x = rA[0]

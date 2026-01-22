ee06f4c803cabc04635c963799f42b081bc6b307
Cole Wilson <colewilson.main@gmail.com>
2026-01-22 13:23:29 -0800

    fix: change the teleop launch file

--- src/guppy_teleop/setup.py
+++ src/guppy_teleop/setup.py
@@ -11,7 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.[pxy][yma]*')))
     ],
     install_requires=['setuptools', 'pygame'],
     zip_safe=True,

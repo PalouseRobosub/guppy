1a6f05a1c40186b54f92de4ecad882b43860d4f4
B0gos <admin@perhapsbot.dev>
2026-02-09 18:54:58 -0800

    feat: added scripts to record/play back ros bags

---
+++ util/play_bag.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Replays the ros bag saved as guppy
+ros2 bag info guppy
+ros2 bag play guppy
---
+++ util/record_bag.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Records everything posted to ros namespace into a ros bag named guppy.bag
+ros2 bag record -o guppy -a

19a1c0b8d1af060ff45da26cb108288c11dff629
Dylan Dean <230458421+dyloniuss@users.noreply.github.com>
2026-02-28 21:57:10 +0000

    fix: timer constantly publishes state regardless of change.

--- src/guppy_state/src/led_pub.cpp
+++ src/guppy_state/src/led_pub.cpp
@@ -9,24 +9,33 @@
 class LEDStatePublisher : public rclcpp::Node
 {
 public:
-  LEDStatePublisher() : Node("led_state")
+  LEDStatePublisher() : Node("led_pub")
   {
     auto topic_callback =
       [this](std_msgs::msg::UInt8::UniquePtr msg) -> void {
-        auto request = std::make_shared<guppy_msgs::srv::SendCan::Request>();
-        request->id = 0x201;
-        std::vector<uint8_t> bytes = { msg->data };
-        request->data = bytes;
-        client_->async_send_request(request);
+        current_state = msg->data;
       };
 
-   
     subscription_ = this->create_subscription<std_msgs::msg::UInt8>("state", 10, topic_callback);
     client_ = this->create_client<guppy_msgs::srv::SendCan>("can_tx");
+
+
+    timer = this->create_wall_timer(
+      std::chrono::milliseconds(100),
+      [this](){
+        auto request = std::make_shared<guppy_msgs::srv::SendCan::Request>();
+        request->id = 0x201;
+        request->data = { current_state };
+        client_->async_send_request(request);
+      
+      }
+    );
   }
 private:
+  uint8_t current_state = 0;
   rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscription_;
   rclcpp::Client<guppy_msgs::srv::SendCan>::SharedPtr client_;
+  rclcpp::TimerBase::SharedPtr timer;
 };
 
 int main(int argc, char * argv[])

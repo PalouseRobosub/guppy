89f7a82c5e8f0d1a275b9acd0877c2cb5879ffea
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2026-04-07 12:13:07 -0700

    feat: threaded cams

--- src/guppy_vision/src/camera_pub.cpp
+++ src/guppy_vision/src/camera_pub.cpp
@@ -1,34 +1,50 @@
-#include <memory>
-#include <atomic>
+#include <CameraDefs.h>
 #include <Spinnaker.h>
+#include <atomic>
+#include <iostream>
+#include <memory>
+#include <string>
 #include <thread>
+#include <vector>
 // #include <SpinGenApi/SpinnakerGenApi.h>
 
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
 #include "sensor_msgs/fill_image.hpp"
+#include "sensor_msgs/msg/image.hpp"
 
 class CameraPublisher : public rclcpp::Node {
-public:
+ public:
   CameraPublisher() : Node("camera_publisher") {
     this->running_.store(true);
 
-    this->publisher_ = this->create_publisher<sensor_msgs::msg::Image>("cam0", 1);
+    // this->publisher_ = this->create_publisher<sensor_msgs::msg::Image>("cam0", 1);
 
-    this->system  = Spinnaker::System::GetInstance();
+    this->system = Spinnaker::System::GetInstance();
     this->camList = system->GetCameras();
 
-    this->ackThread = std::thread(&CameraPublisher::acquireFootage, this);
-
+    for (unsigned int i = 0; i < camList.GetSize(); ++i) {
+      std::string name = "cam";
+      name += i;
+      std::cout << name << std::endl;
+      this->publishers_.push_back(this->create_publisher<sensor_msgs::msg::Image>(name, 1));
+      this->threads_.emplace_back(&CameraPublisher::acquireFootage, this, i);
+    }
   }
 
   ~CameraPublisher() {
     this->running_.store(false);
+
+    for (auto& t : threads_) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+
     this->camList.Clear();
     this->system->ReleaseInstance();
   }
 
-private:
+ private:
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
 
   Spinnaker::SystemPtr system;
@@ -37,36 +53,29 @@ private:
   std::thread ackThread;
   std::atomic<bool> running_{false};
 
-  void acquireFootage() {
-    Spinnaker::CameraPtr pCam = this->camList[0];
+  std::vector<rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr> publishers_;
+  std::vector<std::thread> threads_;
+
+  void acquireFootage(int camIndex) {
+    Spinnaker::CameraPtr pCam = this->camList[camIndex];
     pCam->Init();
     pCam->BeginAcquisition();
     Spinnaker::ImageProcessor processor;
     while (this->running_) {
       Spinnaker::ImagePtr pImg = pCam->GetNextImage(1000);
-      Spinnaker::ImagePtr pProcessed = processor.Convert(pImg, Spinnaker::PixelFormat_RGB8);
-      sensor_msgs::msg::Image imageMsg;
-      sensor_msgs::fillImage(
-        imageMsg,
-        "rgb8",
-        pProcessed->GetHeight(),
-        pProcessed->GetWidth(),
-        pProcessed->GetStride(),
-        pProcessed->GetData()
-        );
+      Spinnaker::ImagePtr pProcessed = processor.Convert(pImg, Spinnaker::PixelFormat_Mono8);
+      auto msg = std::make_unique<sensor_msgs::msg::Image>();
+      sensor_msgs::fillImage(*msg, "mono8", pProcessed->GetHeight(), pProcessed->GetWidth(), pProcessed->GetStride(), pProcessed->GetData());
+      pImg->Release();
 
-      std::cout << "publish" << std::endl;
-
-      this->publisher_->publish(imageMsg);
+      this->publishers_[camIndex]->publish(std::move(msg));
     }
   }
-
 };
 
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<CameraPublisher>());
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char* argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CameraPublisher>());
+  rclcpp::shutdown();
+  return 0;
 }

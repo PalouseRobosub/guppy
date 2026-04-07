41a0fcac8b8c5afbf12525d1f7f1164206539281
B0gos <admin@perhapsbot.dev>
2026-04-06 19:50:08 -0700

    feat: added msg type to subscribers

--- src/guppy_safety/src/checker.cpp
+++ src/guppy_safety/src/checker.cpp
@@ -1,9 +1,16 @@
 #include <memory>
+#include <iostream>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 
-// Checks a bunch of topics, and sets a status 
+#include "std_msgs/msg/string.hpp"
+#include "guppy_msgs/msg/state.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+//#include "nav_msgs/msg/Odometry.msg"
+
+// Checks a bunch of topics, and sets a safety level
 // if one or more of them shows an unhealthy value
 class Checker : public rclcpp::Node
 {
@@ -11,49 +18,78 @@ public:
   Checker()
   : Node("safety_checker")
   {
-    // Publish result of check as a status
-    //auto updateStatus = {
-
-    //};
-
-    // v Check topic callbacks v
+    // Publish result of check as a level
+    pub_level = this->create_publisher<std_msgs::msg::String>("safety_level", 10);
+  
+    current_level = 2;
+    publish_status(5);
 
     // CAN RX timeout
-    auto check_CAN    = [this](std_msgs::msg::String::UniquePtr msg) -> void {
+    /* auto check_CAN    = [this](std_msgs::msg::String::UniquePtr msg) -> void {
         RCLCPP_INFO(this->get_logger(), "[can_rx] '%s'", msg->data.c_str());
-    };
+        update_status(5);
+        printf("can_rx ran");
+    }; */
 
     // Impossible Odom
-    auto check_Odom   = [this](std_msgs::msg::String::UniquePtr msg) -> void {
+    /* auto check_Odom   = [this](nav_msgs::msg::Odometry::UniquePtr msg) -> void {
       RCLCPP_INFO(this->get_logger(), "[odom] '%s'", msg->data.c_str());
-    };
+         printf("odom ran");    update_status(5); 
+    }; */
 
     // Excessive Commaned Velocity 
-    auto check_CmdVel = [this](std_msgs::msg::String::UniquePtr msg) -> void {
-      RCLCPP_INFO(this->get_logger(), "[cmd_vel] '%s'", msg->data.c_str());  
+    auto check_CmdVel = [this](geometry_msgs::msg::Twist::UniquePtr msg) -> void {
+      //RCLCPP_INFO(this->get_logger(), "[cmd_vel] '%s'", msg->data.c_str());
+      update_status(7); 
     };
 
     // Fault state already set somewhere else
-    auto check_state  = [this](std_msgs::msg::String::UniquePtr msg) -> void { 
-      RCLCPP_INFO(this->get_logger(), "[state] '%s'", msg->data.c_str());
+    auto check_state  = [this](guppy_msgs::msg::State::UniquePtr msg) -> void { 
+      //RCLCPP_INFO(this->get_logger(), "[state] '%s'", msg->data.c_str());
+      update_status(6); 
     };
 
     // Subscibe to all checked topics
-    sub_canrx  = this->create_subscription<std_msgs::msg::String>("can_rx",  10, check_CAN);
-    sub_odom   = this->create_subscription<std_msgs::msg::String>("odom",    10, check_Odom);
-    sub_cmdvel = this->create_subscription<std_msgs::msg::String>("cmd_vel", 10, check_CmdVel);
-    sub_state  = this->create_subscription<std_msgs::msg::String>("state",   10, check_state);
-  }
+    //sub_canrx  = this->create_subscription<std_msgs::msg::String>(     "can_rx",  10, check_CAN);
+    //sub_odom   = this->create_subscription<nav_msgs::msg::Odometry>(   "odom",    10, check_Odom);
+    sub_cmdvel = this->create_subscription<geometry_msgs::msg::Twist>( "cmd_vel", 10, check_CmdVel);
+    sub_state  = this->create_subscription<guppy_msgs::msg::State>(    "state",   10, check_state);
+  };
 
 private:
-  // Status
-  int status = 5;
+  // Store current 
+  int current_level;
+
+  // Publisher
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_level;
 
   // All subsciption types
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_canrx;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_odom;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_cmdvel;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_state;
+  //rclcpp::Subscription<guppy_msgs::msg::can_frame>::SharedPtr sub_canrx;
+  //rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_odom;
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_cmdvel;
+  rclcpp::Subscription<guppy_msgs::msg::State>::SharedPtr sub_state;
+ 
+  // Publish the current level 
+  // (To be called in each checker callback)
+  void update_status(int new_level) {
+    // Check if new is more severe than old
+    if (new_level > current_level) {
+        publish_status(new_level);
+    }; 
+  };
+
+  void publish_status(int new_level) {
+        // Move the the new value into current
+        current_level = new_level;
+
+        // Create a message
+        auto message = std_msgs::msg::String();
+        message.data = std::to_string(current_level);
+      
+        // Publish that new level
+        RCLCPP_INFO(this->get_logger(), "[Published Level] '%s'", message.data.c_str());
+        this->pub_level->publish(message);
+  };
 };
 
 int main(int argc, char * argv[])

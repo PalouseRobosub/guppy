74b11f6414ac401292ea8d38e01af7f6b147c58c
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-04-26 01:10:08 -0700

    fix: remove libs

--- src/guppy_vision/src/signs.cpp
+++ src/guppy_vision/src/signs.cpp
@@ -10,8 +10,6 @@
 #include "opencv2/opencv.hpp"
 #include <opencv2/core.hpp>
 #include <opencv2/core/types.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <vector>
 
